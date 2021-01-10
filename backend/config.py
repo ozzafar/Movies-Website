f7ff465669a01ad73f47ac56d583902a926bc8cb
Dvir Salomon <46302230+dvir1@users.noreply.github.com>
2021-01-10 16:59:50 +0200

    Change config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 DEBUG = True
 PORT = 40006
 LOCALHOST = '127.0.0.1'
-WEBSERVER = 'delta-tomcat-vm.cs.tau.ac.il'
+WEBSERVER = '0.0.0.0'
 DB_PORT = 3306
 DB_HOST = 'mysqlsrv1.cs.tau.ac.il'
 REMOTE = True
