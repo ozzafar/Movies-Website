77141c97740a07fd6e3f59dd3c8baed12d01ad80
MorPo <62566869+MorPo@users.noreply.github.com>
2021-01-15 21:42:34 +0200

    database

--- backend/insert_data_2_db/DBConstructor.py
+++ backend/insert_data_2_db/DBConstructor.py
@@ -40,99 +40,115 @@ class DBConstructor:
 
     # region INSERT
 
-    def insert_movie(self, mv):
+    def insert_movie_table(self, tmdb_mv, omdb_mv):
         cursor = self.cnx.cursor()  # get the cursor
-        sql = "INSERT INTO Movies100 (movie_ID,title,released,run_time,plot,budget,revenue,poster_URL,trailer_URL) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        runtime = mv.runtime
-        if runtime is not None:
-            runtime = '{:02d}:{:02d}'.format(*divmod(mv.runtime, 60))
-        if runtime == '':
-            runtime = None
-        budget = mv.budget
-        if budget == 0:
-            budget = None
-        revenue = mv.revenue
-        if revenue == 0:
+        sql = "INSERT INTO Movies (movie_ID, title, rated_id, released, run_time, plot, awards, budget, revenue, " \
+              "poster_URL, trailer_URL) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        rated_id = self.__get_rated_id(omdb_mv)
+        runtime = '{:02d}:{:02d}'.format(*divmod(tmdb_mv.runtime, 60))
+        plot = self.__get_plot(tmdb_mv, omdb_mv)
+        awards = self.__get_awards(omdb_mv)
+        revenue = tmdb_mv.revenue
+        if revenue is None or revenue == 0:
             revenue = None
-        overview = mv.overview
-        if overview == "" or overview == '':
-            overview = None
-        release_date = mv.release_date
-        if release_date == '':
-            release_date = None
-        poster = mv.poster_path
-        if poster == '':
+        poster = tmdb_mv.poster_path
+        if poster is None or poster == '':
             poster = None
-        trailer = mv.trailers
-        if trailer != None and "youtube" in trailer:
+        trailer = tmdb_mv.trailers
+        if trailer is not None and "youtube" in trailer:
             trailer = trailer["youtube"]
             if len(trailer) > 0:
                 trailer = trailer[0]["source"]
             else:
                 trailer = None
-        values = (mv.id, mv.title, release_date, runtime, overview, budget, revenue, poster, trailer)
+        values = (tmdb_mv.id, tmdb_mv.title, rated_id, tmdb_mv.release_date, runtime,
+                  plot, awards, tmdb_mv.budget, revenue, poster, trailer)
         cursor.execute(sql, values)
         self.cnx.commit()
+
+    def __get_rated_id(self, omdb_mv):
+        content_rating = {"G": 1, "PG": 2, "PG-13": 3, "R": 4, "NC-17": 5}
+        if omdb_mv["Rated"] in content_rating:
+            return content_rating[omdb_mv["Rated"]]
+        return None
+
+    def __get_plot(self, tmdb_mv, omdb_mv):
+        omdb_plot = omdb_mv['Plot']
+        if omdb_plot is not None and len(omdb_plot) > 5:
+            return omdb_plot
+        return tmdb_mv.overview
+
+    def __get_awards(self, omdb_mv):
+        tmp = omdb_mv['Awards']
+        if "wins" in tmp:
+            tmp = tmp.split()
+            if "wins" in tmp:
+                wins = tmp[tmp.index("wins") - 1]
+            elif "wins." in tmp:
+                wins = tmp[tmp.index("wins.") - 1]
+            else:
+                wins = 0
+            if wins.isnumeric():
+                return int(wins)
+        return 0
 
     def insert_rating(self, rating):
-        cursor = self.cnx.cursor()  # get the cursor
-        sql = "INSERT INTO Rated1 (rated) VALUES (%s)"
+        sql = "INSERT INTO Rated (rated) VALUES (%s)"
         values = (rating,)
-        cursor.execute(sql, values)
-        self.cnx.commit()
+        self.execute_sql(sql, values)
 
     def insert_genre(self, g):
-        sql = "INSERT INTO Genres1 (genre) VALUES (%s)"
+        sql = "INSERT INTO Genres (genre) VALUES (%s)"
         values = (g,)
         self.execute_sql(sql, values)
 
     def insert_countrie(self, c):
-        sql = "INSERT INTO Production_Countries1 (country) VALUES (%s)"
+        sql = "INSERT INTO Production_Countries (country) VALUES (%s)"
         values = (c,)
         self.execute_sql(sql, values)
 
     def insert_movies_genres(self, m_id, g_id):
-        sql = "INSERT INTO Movie_Genres1 (movie_ID,genre_ID) VALUES (%s,%s)"
+        sql = "INSERT INTO Movie_Genres (movie_ID,genre_ID) VALUES (%s,%s)"
         values = (m_id, g_id)
         self.execute_sql(sql, values)
 
     def insert_movies_companies(self, m_id, c_id):
-        sql = "INSERT INTO Movie_Companies1 (movie_ID,prod_company_ID) VALUES (%s,%s)"
+        sql = "INSERT INTO Movie_Companies (movie_ID,prod_company_ID) VALUES (%s,%s)"
         values = (m_id, c_id)
         self.execute_sql(sql, values)
 
     def insert_company(self, c):
-        sql = "INSERT INTO Production_companies1 (company) VALUES (%s)"
+        sql = "INSERT INTO Production_companies (company) VALUES (%s)"
         values = (c,)
         self.execute_sql(sql, values)
 
-    def insert_actor(self, first, last, gender, profile_path):
-        sql = "INSERT INTO Person1 (first_name,last_name,gender,picture_URL) VALUES (%s,%s,%s,%s)"
-        values = (first, last, gender, profile_path)
-        self.execute_sql(sql, values)
-
     def insert_country(self, c):
-        sql = "INSERT INTO Production_Countries1 (country) VALUES (%s)"
+        sql = "INSERT INTO Production_Countries (country) VALUES (%s)"
         values = (c,)
         self.execute_sql(sql, values)
 
     def insert_movies_countries(self, m_id, c_id):
-        sql = "INSERT INTO Movie_Countries1 (movie_ID,prod_country_ID) VALUES (%s,%s)"
+        sql = "INSERT INTO Movie_Countries (movie_ID,prod_country_ID) VALUES (%s,%s)"
         values = (m_id, c_id)
         self.execute_sql(sql, values)
 
+    def insert_person(self, first, last, gender, profile_path):
+        sql = "INSERT INTO Person (first_name,last_name,gender,picture_URL) VALUES (%s,%s,%s,%s)"
+        values = (first, last, gender, profile_path)
+        self.execute_sql(sql, values)
+
     def insert_movies_actors(self, m_id, c_id, character):
-        sql = "INSERT INTO Movies_Actors1 (person_ID,movie_ID,figure) VALUES (%s,%s,%s)"
+        sql = "INSERT INTO Movies_Actors (person_ID,movie_ID,figure) VALUES (%s,%s,%s)"
         values = (c_id, m_id, character)
         self.execute_sql(sql, values)
 
     def insert_movies_crew(self, m_id, c_id, job):
-        sql = "INSERT INTO Movies_Crew1 (person_ID,movie_ID,role) VALUES (%s,%s,%s)"
+        sql = "INSERT INTO Movies_Crew (person_ID,movie_ID,role) VALUES (%s,%s,%s)"
         values = (c_id, m_id, job)
         self.execute_sql(sql, values)
 
     def insert_score(self, m_id, imdb, rotten_tomatoes, metacritic, imdbVotes):
-        sql = "INSERT INTO Movie_Score1 (movie_ID,rotten_tomatoes,metacritic,imdb,imdbVotes) VALUES (%s,%s,%s,%s,%s)"
+        sql = "INSERT INTO Movie_Score (movie_ID,rotten_tomatoes,metacritic,imdb,imdbVotes) VALUES (%s,%s,%s,%s,%s)"
         values = (m_id, rotten_tomatoes, metacritic, imdb, imdbVotes)
         self.execute_sql(sql, values)
 
@@ -141,12 +157,12 @@ class DBConstructor:
     # region UPDATE
 
     def update_poster(self, url, id):
-        sql = "UPDATE Movies100 SET poster_URL=%s WHERE movie_ID = %s"
+        sql = "UPDATE Movies SET poster_URL=%s WHERE movie_ID = %s"
         values = (url, id)
         self.execute_sql(sql, values)
 
     def update_movie(self, m_id, awards, rated):
-        sql = "UPDATE Movies100 SET awards=%s, rated_ID=%s WHERE movie_ID = %s"
+        sql = "UPDATE Movies SET awards=%s, rated_ID=%s WHERE movie_ID = %s"
         values = (awards, rated, m_id)
         self.execute_sql(sql, values)
 
@@ -156,18 +172,42 @@ class DBConstructor:
 
     def get_genres(self):
         cursor = self.cnx.cursor()  # get the cursor
-        sql ="SELECT * FROM Genres1"
+        sql = "SELECT * FROM Genres"
         cursor.execute(sql)
         return cursor.fetchall()
 
     def get_countries(self):
         cursor = self.cnx.cursor()  # get the cursor
-        sql ="SELECT * FROM Production_Countries1"
+        sql = "SELECT * FROM Production_Countries"
         cursor.execute(sql)
         return cursor.fetchall()
 
     def get_movie_ids(self):
         cursor = self.cnx.cursor()  # get the cursor
-        sql = "SELECT movie_ID FROM Movies100"
+        sql = "SELECT movie_ID FROM Movies"
+        cursor.execute(sql)
+        return cursor.fetchall()
+
+    def get_company_id(self, company):
+        cursor = self.cnx.cursor()  # get the cursor
+        sql = f"""SELECT prod_company_ID FROM Production_companies WHERE company = "{str(company)}" """
+        cursor.execute(sql)
+        return cursor.fetchall()
+
+    def get_all_company_data(self):
+        cursor = self.cnx.cursor()  # get the cursor
+        sql = f"""SELECT * FROM Production_companies"""
+        cursor.execute(sql)
+        return cursor.fetchall()
+
+    def get_person_id(self, first_name, last_name):
+        cursor = self.cnx.cursor()  # get the cursor
+        sql = f"""SELECT person_ID FROM Person WHERE first_name = "{str(first_name)}" AND last_name = "{str(last_name)}" """
+        cursor.execute(sql)
+        return cursor.fetchall()
+
+    def get_all_person_data(self):
+        cursor = self.cnx.cursor()  # get the cursor
+        sql = f"""SELECT * FROM Person"""
         cursor.execute(sql)
         return cursor.fetchall()
--- backend/insert_data_2_db/movies.py
+++ backend/insert_data_2_db/movies.py
@@ -1,23 +1,19 @@
-import json
-
-import omdb
-# from tmdbv3api import *
 import tmdbv3api
-from omdbapi.movie_search import GetMovie
+import requests
 from DBConstructor import DBConstructor
 import mysql.connector
+from datetime import datetime as dt
 
 movie_titles = set()
 db = DBConstructor()
 api_key = "b1319d9182fbc16b74ad682f29067d21"
+# omdb_api_key = '5987dc54'
+omdb_api_key = '74fb7c6d'
 content_rating = {"G": 1, "PG": 2, "PG-13": 3, "R": 4, "NC-17": 5}
+errors = {"budget": 0, "genres": 0, "overview": 0, "runtime": 0, "title": 0, "imdb_id": 0, "score": 0}
 
 # ------------------ get popular movies titles by TMDB API ------------------
-#tmdb = TMDb()
-#tmdb.api_key = "b1319d9182fbc16b74ad682f29067d21"
-#movie = Movie()
-flag = False
-errors = 0
+
 
 class FillingTables:
     def __init__(self):
@@ -25,9 +21,7 @@ class FillingTables:
         self._companies = dict()
         self._countries = dict()
         self._persons = dict()
-
-    def __check_if_all_movie_info_exists():
-        pass
+        self._all_movies_id = set()
 
     def fill_genres_table(self):
         genres = tmdbv3api.Genre()
@@ -48,152 +42,230 @@ class FillingTables:
             except mysql.connector.errors.IntegrityError as err:
                 continue
 
-    def fill_genres_dict(self):
+    def __fill_genres_dict(self):
         for genre in db.get_genres():
             self._genres[genre[1]] = genre[0]
 
-    def fill_countries_dict(self):
+    def __fill_countries_dict(self):
         for country in db.get_countries():
             self._countries[country[1]] = country[0]
 
-    def __get_tmdb_movie_details_generator(self):
+    def __fill_companies_dict(self):
+        for company in db.get_all_company_data():
+            self._companies[company[1].lower()] = company[0]
+
+    def __fill_persons_dict(self):
+        for pers in db.get_all_person_data():
+            self._persons[pers[1] + " " + pers[2]] = pers[0]
+
+    def __fill_movies_ids_set(self):
+        for i in db.get_movie_ids():
+            self._all_movies_id.add(i[0])
+
+    def __put_all_data_in_dict(self):
+        self.__fill_genres_dict()
+        self.__fill_countries_dict()
+        self.__fill_companies_dict()
+        self.__fill_persons_dict()
+        self.__fill_movies_ids_set()
+
+    def __get_tmdb_movie_details_generator(self, stater_page):
         movie = tmdbv3api.Movie()
         movie.api_key = api_key
-        for i in range(1, 500):
-            page = movie.popular(i)
+        for i in range(stater_page, 501):
+            page = None
+            while page is None:
+                try:
+                    page = movie.popular(i)
+                except:
+                    print("---------------connection failed 60---------------")
             for j in range(20):
-                yield movie.details(page[j].id), i, j
+                details = None
+                while details is None:
+                    try:
+                        details = movie.details(page[j].id)
+                    except:
+                        print("---------------connection failed 67---------------")
+                yield details, i, j
 
-    def fill_all_the_tables(self):
-        tmdb_movie_generator = self.__get_tmdb_movie_details_generator()
-        while True:
-            tmdb_movie = next(tmdb_movie_generator)
-            if not self.__check_if_all_movie_info_exists():
+    def fill_all_the_tables(self, stater_page, cr):
+        self.__put_all_data_in_dict()
+        tmdb_movie_generator = self.__get_tmdb_movie_details_generator(stater_page)
+        counter = cr
+        num_of_spinning = 0  # TODO DELETE
+        while counter < 1000:
+            print("numOfSpining: " + str(num_of_spinning))  # TODO DELETE
+            num_of_spinning += 1  # TODO DELETE
+            print("counter: " + str(counter))  # TODO DELETE
+            print("errors: " + str(errors))  # TODO DELETE
+            tmdb_movie, page, index = next(tmdb_movie_generator)  # get next movie
+            if tmdb_movie.id in self._all_movies_id:
                 continue
+            if not self.__fill_movie_related_table(tmdb_movie):
+                continue
+            self.__fill_credit_related_table(tmdb_movie.id)
+            self._all_movies_id.add(tmdb_movie.id)
+            counter += 1
 
-# for i in range(1,50):
-#     movies = movie.popular(i)
-#     for mv in movies:
-#         try:
-#             mv = movie.details(mv.id)
-#             db.insert_movie(mv)
-#         except Exception as e:
-#             print(e)
-#             errors += 1
-#     print("finished page #" + str(i) + " of " + str(60))
-# print("finished insert all movies, total errors number: " + str(errors))
+    def __fill_movie_related_table(self, tmdb_movie):
+        # Check that all data exists
+        if not self.__check_if_all_tmdb_movie_info_is_valid(tmdb_movie):
+            print("__check_if_all_tmdb_movie_info_is_valid: " + str(tmdb_movie.title) + " " + str(tmdb_movie.id))  # TODO DELETE
+            return False
+        payload = {'i': tmdb_movie.imdb_id, 'plot': 'full', 'r': 'json', 'apikey': omdb_api_key}
+        omdb_movie = None
+        while omdb_movie is None:
+            try:
+                omdb_movie = requests.get('http://www.omdbapi.com/', params=payload).json()
+            except:
+                print("---------------connection failed 96---------------")
+        if not self.__check_if_score_data_exists(omdb_movie):
+            print("__check_if_score_data_exists: " + str(omdb_movie["Title"]) + " " + str(omdb_movie["imdbID"]))  # TODO DELETE
+            errors["score"] += 1  # TODO DELETE
+            return False
 
-#
-# if flag:
-#     moviesss = db.get_movie_ids()
-#     for i in range(len(moviesss)):
-#         m = movie.details(moviesss[i][0])
-#         m2 = GetMovie(title=m.title, api_key='5987dc54')
-#         try:
-#             for country in m.production_countries:
-#                 name = country["name"]
-#                 if name not in countries:
-#                     db.insert_country(name)
-#                     countries[name] = len(countries)+1
-#                 db.insert_movies_countries(m.id, countries[name])
-#         except Exception as e:
-#             print(e)
-#             errors += 1
-#         try:
-#             for comp in m.production_companies:
-#                 name = comp["name"]
-#                 if name not in companies:
-#                     db.insert_company(name)
-#                     companies[name] = len(companies)+1
-#                 db.insert_movies_companies(m.id, companies[name])
-#         except Exception as e:
-#             print(e)
-#             errors += 1
-#
-#         try:
-#             for g in m.genres:
-#                 name = g["name"]
-#                 if name not in genres:
-#                     db.insert_genre(name)
-#                     genres[name] = len(genres)+1
-#                 db.insert_movies_genres(m.id, genres[name])
-#         except Exception as e:
-#             print(e)
-#             errors += 1
-#         try:
-#             tomatoes = None
-#             for s in m2.values["Ratings"]:
-#                 if s["Source"]=="Rotten Tomatoes":
-#                     tomatoes = int(s["Value"][:-1])
-#                     break
-#             metascore = None
-#             if "Metascore" in m2.values and m2.values["Metascore"]!= "N/A":
-#                 metascore = int(m2.values["Metascore"])
-#             db.insert_score(m.id, int(10 * float(m2.values["imdbRating"])),tomatoes, metascore, int(m2.values["imdbVotes"].replace(',', '')))
-#             rated = None
-#             if "Rated" in m2.values and m2.values["Rated"] != "N/A":
-#                 rated = content_rating[m2.values["Rated"]]
-#             db.update_movie(m.id, None, rated) # todo what about awards ?
-#         except Exception as e:
-#             print(e)
-#             errors += 1
-#
-#         try:
-#             for j in range(min(7, len(m.casts["cast"]))):
-#                 person = m.casts["cast"][j]
-#                 if person["original_name"] not in persons:
-#                     res = person["original_name"].split()
-#                     first = res[0]
-#                     if len(res) > 1:
-#                         last = " ".join(res[1:])
-#                     else:
-#                         last = ""
-#                     db.insert_actor(first, last, person["gender"], person["profile_path"])
-#                     persons[person["original_name"]] = len(persons) + 1
-#                 db.insert_movies_actors(m.id, persons[person["original_name"]], person["character"])
-#         except Exception as e:
-#             print(e)
-#             errors += 1
-#         try:
-#             for crew in m.casts["crew"]:
-#                 if crew["job"] == "Director" or crew['job'] == "Writer":
-#                     if crew["original_name"] not in persons:
-#                         res = crew["original_name"].split()
-#                         first = res[0]
-#                         if len(res) > 1:
-#                             last = " ".join(res[1:])
-#                         else:
-#                             last = ""
-#                         db.insert_actor(first, last, crew["gender"], crew["profile_path"])
-#                         persons[crew["original_name"]] = len(persons) + 1
-#                     db.insert_movies_crew(m.id, persons[crew["original_name"]], crew["job"])
-#         except Exception as e:
-#             print(e)
-#             errors += 1
-#
-#         print("finished movie #" + str(i) + " of " + str(len(moviesss)))
-#     print("finished insert all movies, total errors number: " + str(errors))
-#
-#     # ------------------ get popular movies data by OMDB API ------------------
-#     i = 0
-#     omdb.set_default("apikey", "5987dc54")
-#     for title in movie_titles:
-#         content_bytes = omdb.request(t=title).content
-#         mv = json.loads(content_bytes)
-#         movies.append(mv)
-#         if i % 100 == 0:
-#             print("finishedmovies_tmdb movie #" + str(i) + " of " + str(20))
-#         #     print("finishedmovies_tmdb movie #" + str(i) + " of " + str(20 * pages))
-#         i += 1
-#
-#     # ------------------ fill all tables in DB ------------------
+        # fill the tables
+        db.insert_movie_table(tmdb_movie, omdb_movie)
+        self.__insert_movies_genres_table(tmdb_movie)
+        self.__insert_movies_countries_table(tmdb_movie)
+        self.__insert_companies_tables(tmdb_movie)
+        self.__insert_movie_score_table(omdb_movie, tmdb_movie)
+        return True
+
+    def __check_if_all_tmdb_movie_info_is_valid(self, tmdb_movie):
+        if not self.__is_date_valid(tmdb_movie.release_date):
+            return False
+        elif tmdb_movie.budget is None or tmdb_movie.budget < 10000:
+            errors["budget"] += 1  # TODO DELETE
+            return False
+        elif tmdb_movie.genres is None or len(tmdb_movie.genres) == 0:
+            errors["genres"] += 1  # TODO DELETE
+            return False
+        elif tmdb_movie.overview is None or tmdb_movie.overview == "" or tmdb_movie.overview == " ":
+            errors["overview"] += 1  # TODO DELETE
+            return False
+        elif tmdb_movie.runtime is None or tmdb_movie.runtime < 60:
+            errors["runtime"] += 1  # TODO DELETE
+            return False
+        elif tmdb_movie.title is None or tmdb_movie.title == "" or tmdb_movie.title == " " or \
+                len(tmdb_movie.title) >= 200:
+            errors["title"] += 1  # TODO DELETE
+            return False
+        elif tmdb_movie.imdb_id is None or tmdb_movie.imdb_id == "" or tmdb_movie.imdb_id == " ":
+            errors["imdb_id"] += 1  # TODO DELETE
+            return False
+        return True
+
+    def __is_date_valid(self, release_date):
+        max_date = dt.strptime("2021-1-1", "%Y-%m-%d")
+        if release_date is None or release_date == '' or max_date < dt.strptime(release_date, "%Y-%m-%d"):
+            return False
+        return True
+
+    def __check_if_score_data_exists(self, omdb_movie):
+        score = omdb_movie["Ratings"]
+        if len(score) < 3:
+            return False
+        elif(score[0]['Value'] == '' or score[0]['Value'] is None or
+             score[1]['Value'] == '' or score[1]['Value'] is None or
+             score[2]['Value'] == '' or score[2]['Value'] is None):
+            return False
+        return True
+
+    def __insert_movies_genres_table(self, tmdb_movie):  # len(tmdb_movie.genres) > 0
+        for g in tmdb_movie.genres:
+            name = g["name"]
+            db.insert_movies_genres(tmdb_movie.id, self._genres[name])
+
+    def __insert_movies_countries_table(self, tmdb_movie):
+        for country in tmdb_movie.production_countries:
+            name = country["name"]
+            db.insert_movies_countries(tmdb_movie.id, self._countries[name])
+
+    def __insert_companies_tables(self, tmdb_movie):
+        for comp in tmdb_movie.production_companies:
+            name = comp["name"]
+            if name.lower() not in self._companies:
+                db.insert_company(name)
+                self._companies[name.lower()] = db.get_company_id(name)[0][0]
+            db.insert_movies_companies(tmdb_movie.id, self._companies[name.lower()])
+
+    def __insert_movie_score_table(self, omdb_movie, tmdb_movie):
+        for s in omdb_movie["Ratings"]:
+            if s["Source"] == 'Internet Movie Database':
+                imdb_rating = int(10 * float(s["Value"][:-3]))
+            elif s["Source"] == "Rotten Tomatoes":
+                tomatoes_rating = int(s["Value"][:-1])
+            elif s["Source"] == "Metacritic":
+                metacritic_rating = int(s["Value"][:-4])
+        db.insert_score(tmdb_movie.id, imdb_rating, tomatoes_rating, metacritic_rating,
+                        int(omdb_movie["imdbVotes"].replace(',', '')))
+
+    def __fill_credit_related_table(self, movie_id):
+        movie = tmdbv3api.Movie()
+        movie.api_key = api_key
+        staff = None
+        while staff is None:
+            try:
+                staff = movie.credits(movie_id)
+            except:
+                print("---------------connection failed 186---------------")
+        self.__fill_actors_and_person_tables(staff["cast"], movie_id)
+        self.__fill_crew_and_persons_table(staff["crew"], movie_id)
+
+    def __fill_actors_and_person_tables(self, cast, movie_id):
+        counter = 0
+        for ac in cast:
+            if counter >= 7:
+                break
+            name = ac.name
+            if name is None or len(name.split()) < 2 or len(name.split()[0]) >= 20 or len(name.split()[1]) >= 20:
+                continue
+            if name not in self._persons:
+                self.__fill_person_table_and_dict(ac)
+            figure = ac.character
+            if figure is None or len(figure) >= 100:
+                figure = ''
+
+            db.insert_movies_actors(movie_id, self._persons[name], figure)
+            counter += 1
+
+    def __fill_crew_and_persons_table(self, crew, movie_id):
+        director_flag = False
+        writer_flag = False
+
+        for cr in crew:
+            if director_flag and writer_flag:
+                break
+            job = cr.job
+            if job is not None and ((not director_flag and job == "Director") or (not writer_flag and job == "Writer")):
+                name = cr.name
+                if name is None or len(name.split()) < 2:
+                    continue
+                if name not in self._persons:
+                    self.__fill_person_table_and_dict(cr)
+                db.insert_movies_crew(movie_id, self._persons[name], job)
+                if job == "Director":
+                    director_flag = True
+                else:
+                    writer_flag = True
+
+    def __fill_person_table_and_dict(self, per):
+        gender = per.gender
+        if type(gender) is not int or (gender != 1 and gender != 2):
+            gender = 0
+        poster = per.profile_path
+        if poster is None or len(poster) < 3:
+            poster = None
+        db.insert_person(per.name.split()[0], per.name.split()[1], gender, poster)
+        self._persons[per.name] = db.get_person_id(per.name.split()[0], per.name.split()[1])[0][0]
+
 
 fill_it = FillingTables()
 # fill_it.fill_genres_table()
 # fill_it.fill_rated_table()
 # fill_it.fill_production_countries_table()
 
-fill_it.fill_genres_dict()
-fill_it.fill_countries_dict()
+fill_it.fill_all_the_tables(91, 902)
 
 db.close()
